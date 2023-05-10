c4a273b4ca6c43d97dd5617c471e2246c5436c72
Coaster <56899612+CoasterFan5@users.noreply.github.com>
2023-05-10 15:54:52 -0400

    Started on proxy

---
+++ src/routes/proxy/+page.svelte
@@ -0,0 +1,75 @@
+
+<script>
+	let url = "";
+	import { goto } from "$app/navigation";
+
+	let searchIt = () => {
+		if (url) {
+			goto(`/proxy/https://google.com/search?q=${url}`)
+		} else {
+			alert("Please enter a URL");
+		}
+	}
+</script>
+
+<div class="holder">
+	<div class="bar">
+		<input placeholder="Search Blockless" bind:value={url}/>
+		<button on:click={searchIt}>Go</button>
+	</div>
+	
+</div>
+
+
+
+
+<style>
+	.holder {
+		margin-top: 20vh;
+		display: flex;
+		flex-direction: row;
+		align-items: center;
+		justify-content: center;
+	}
+	.holder .bar {
+		display: flex;
+		flex-direction: row;
+		align-items: center;
+		justify-content: center;
+		font-size: 1.5rem;
+	}
+	.bar input {
+		all: unset;
+		color: white;
+		width: 100%;
+		height: 100%;
+		padding: 10px;
+		border: none;
+		border-radius: 50px 0 0 50px;
+		margin-right: 5px;
+		background: transparent;
+		background: linear-gradient(to right, rgba(255, 16, 96, 0.2), rgba(255, 172, 6, 0.2));
+	}
+	.bar input:hover {
+		background: linear-gradient(to right, rgba(255, 16, 96, 0.3), rgba(255, 172, 6, 0.3));
+	}
+	.bar input:focus {
+		outline: none;
+		background: linear-gradient(to right, rgba(255, 16, 96, 0.4), rgba(255, 172, 6, 0.4));
+	}
+
+	.holder button {
+		padding: 10px;
+		border: none;
+		border-radius: 0px 50px 50px 0px;
+		background: transparent;
+		color: #fff;
+		font-size: 1.5rem;
+		background: rgba(255, 172, 6, 0.2);
+		cursor: pointer;
+	}
+	.holder button:hover {
+		background: rgba(255, 172, 6, 0.4);
+	}
+	
+</style>
---
+++ src/routes/proxy/go/+server.js
@@ -0,0 +1,85 @@
+import { json } from '@sveltejs/kit';
+import * as cheerio from 'cheerio';
+
+
+export let GET = async ({url}) => {
+	let ourUrl = "/proxy/go?";
+	//url is the url that we are trying to proxy.
+	let newUrl = url.search.substring(1);
+	console.log(newUrl)
+
+	//get the domain from the proxy url 
+	let domain = "";
+	let dmc = newUrl.indexOf("/", 8);
+	if (dmc == -1) {
+		dmc = newUrl.length;
+	}
+	domain = newUrl.substring(0, dmc);
+	console.log(domain)
+
+	let relativeUrl = newUrl.substring(8 + domain.length);
+
+
+	let fetchrq = await fetch(newUrl);
+	let html = await fetchrq.text();
+	let reqType = fetchrq.headers.get("content-type");
+
+
+	//now we have the html, and things get a little tricky. We need to replace all the links with our own. 
+	//We also need to replace all the scripts with our own.
+	//We also need to replace all the styles with our own.
+	//We also need to replace all the images with our own.
+	//you get it by now.
+
+	if(reqType.startsWith("text/html") == false){
+		return new Response(html, {
+			headers: {
+				'content-type': reqType,
+			}
+		});
+	}
+
+	let fixUrl = (url) => {
+		if (url.startsWith("/")) {
+			return ourUrl + domain + url;
+		}
+		if (url.startsWith(".")) {
+			console.log(url)
+			console.log(ourUrl + domain + relativeUrl + url.substring(1))
+			return ourUrl + domain + relativeUrl + url.substring(1);
+		}
+		return `/proxy/go?${url}`;
+	}
+
+
+	//parse with cherrio to get the html
+	let parsed = cheerio.load(html);
+	parsed('a').each((i, link) => {
+		let href = link.attribs.href;
+		link.attribs.href = fixUrl(href);
+	});
+
+	parsed('script').each((i, link) => {
+		if(link.attribs.src){
+			link.attribs.src = fixUrl(link.attribs.src);
+		}
+	});
+
+	parsed('link').each((i, link) => {
+		if(link.attribs.href){
+			link.attribs.href = fixUrl(link.attribs.href);
+		}
+	});
+
+	parsed('img').each((i, link) => {
+		if(link.attribs.src){
+			link.attribs.src = fixUrl(link.attribs.src);
+		}
+	});
+
+	return new Response(parsed.root().html(), {
+		headers: {
+			'content-type': reqType,
+		}
+	});
+}
